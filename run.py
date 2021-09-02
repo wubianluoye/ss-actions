87645aabb9c8bb9390d968f7859c4185558787ca
wubianluoye <575784835@qq.com>
2021-09-02 15:29:46 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
+import os
 
-# 初始化
-def init:
-  pass
 
-if __name__ == "__main__":
+config = {
+  'url': 'https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7',
+  'headers': {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 FS"
+  },
+  'timeout': 30
+}
+
+
+def init():
+  res = requests.get(config['url'], headers=config['headers'])
+  res.encoding='utf-8'
+  print('http status: ', res.status_code)
+  if res.status_code != 200:
+    print('reinit')
+    return init()
+  
+  html = BeautifulSoup(res.text, 'html.parser')
+  SSRs = html.find_all('p', text='SSR链接：')[0].find_next_siblings('p', limit=5)
+  wirte_file(SSRs)
+
+
+def wirte_file(SSRs):
+  if os.path.exists('./result.txt'):
+    os.remove('./result.txt')
+    print('delete result.txt ok!')
+
+  str = ''
+  for item in SSRs:
+    str = str+item.text + '\n'
+
+  with open('./result.txt', 'w', encoding='utf-8') as f:
+    f.write(str)
+
+  print('write file ok')
+
+
+
+if __name__ == '__main__':
   init()
